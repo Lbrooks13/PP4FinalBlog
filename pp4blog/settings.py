1f15129f05622f1ce757a7b0c1415f32b92505e9
Lbrooks13 <lbrooks1308@gmail.com>
2025-10-03 11:47:21 +0100

    settings.py fix

--- pp4blog/settings.py
+++ pp4blog/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pp4finalblog.herokuapp.com','http://127.0.0.1:8000/']
+ALLOWED_HOSTS = ['pp4blog.herokuapp.com','http://127.0.0.1:8000/']
 
 
 # Application definition
